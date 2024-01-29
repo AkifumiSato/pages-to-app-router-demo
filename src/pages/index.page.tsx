98a8b02ef2e27704c9d926e657ef1656fcbf0a3d
akfm.sato <01047245@CF0286.local>
2024-01-29 12:51:51 +0900

    add sample form

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {useRouter} from "next/router";
+
+type Inputs = {
+  message: string
+}
+
+export default function Home() {
+  const {register, handleSubmit} = useForm<Inputs>()
+  const router = useRouter()
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    await router.push(`/success?message=${data.message}`)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Hello world</title>
+      </Head>
+      <main>
+        <h1>Hello world</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Post form</h2>
+          <div>
+            message: <input type="text" {...register('message')}/>
+          </div>
+          <button>submit</button>
+        </form>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/success/index.page.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+export default function Success() {
+  const router = useRouter()
+
+  return (
+    <>
+      <Head>
+        <title>Success</title>
+      </Head>
+      <main>
+        <h1>Post success</h1>
+        <p>message: `{router.query.message ?? '[message is undefined]'}`</p>
+        <Link href="/">top</Link>
+      </main>
+    </>
+  );
+}
